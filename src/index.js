e59b3c922d693981d20f14bfd47736d1447e3b9b
stylesuxx <stylesuxx@gmail.com>
2015-10-11 03:09:49 +0200

    Added method to build query strings

--- src/index.js
+++ src/index.js
@@ -3,10 +3,25 @@ import rp from 'request-promise';
 class YTS {
   constructor(url = 'https://yts.to/api/v2/') {
     this.apiUrl = url;
+
+    this.buildQuery = function(options) {
+      var queryString = '';
+
+      for(var index in options) {
+        queryString += index + '=' + options[index] + '&'
+      }
+      return queryString
+    }
   }
 
-  findMovie(searchString) {
-    return rp({url: this.apiUrl + 'list_movies.json?query_term=' + searchString, json:true});
+  findMovie(searchString, options = {}) {
+    var query = this.buildQuery(options);
+    var url ='list_movies.json?query_term=' + searchString + '&' + query;
+    return rp({url: this.apiUrl + url, json:true});
+  }
+
+  getDetails(movieId) {
+    return rp({url: this.apiUrl + 'movie_details.json?movie_id=' + movieId, json:true});
   }
 }
 
