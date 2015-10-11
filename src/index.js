762274830e724eea46f5ce29527b9b303f5b588e
stylesuxx <stylesuxx@gmail.com>
2015-10-11 17:23:51 +0200

    Added method to like comment

--- src/index.js
+++ src/index.js
@@ -176,6 +176,17 @@ class YTS {
 
     return rp.post({url: this.apiUrl + url, body: body, json: true});
   }
+
+  likeComment(userKey, commentId, applicationKey) {
+    var url = 'like_comment.json';
+    var body = {
+      user_key: userKey,
+      comment_id: commentId,
+      application_key: applicationKey
+    };
+
+    return rp.post({url: this.apiUrl + url, body: body, json: true});
+  }
 }
 
 export default YTS;
